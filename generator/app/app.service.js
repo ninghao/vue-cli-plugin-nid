a3f7c6c6288eef838318bf61b59b0ba18ed41426
wanghao <wanghao@ninghao.net>
2020-11-23 15:12:49 +0800

    定义获取生成文件完整路径的方法（getGeneratedFilePath）

---
+++ generator/app/app.service.js
@@ -0,0 +1,47 @@
+const path = require('path');
+
+const getGeneratedFilePath = (fileType, options) => {
+  /**
+   * 1. --component comment
+   *    -> src/comment/comment.vue
+   *
+   * 2. --component comment-index
+   *    -> src/comment/index/comment-index.vue
+   *
+   * 3. --component comment-list --path comment/index/components
+   *    -> src/comment/index/components/comment-list.vue
+   */
+
+  const { [fileType]: fileName, path: filePath } = options;
+
+  // 带后缀的文件名
+  let fileFullName;
+
+  switch (fileType) {
+    case 'component':
+      fileFullName = `${fileName}.vue`;
+      break;
+  }
+
+  const fileNameArray = fileName.split('-');
+  const isMultiWordsFile = fileNameArray.length > 1;
+
+  // 文件存放位置
+  let fileFullPath = [];
+
+  if (filePath) {
+    // 3
+    const filePathArray = filePath.split('/');
+    fileFullPath = ['src', ...filePathArray, fileFullName];
+  } else if (isMultiWordsFile) {
+    // 2
+    fileFullPath = ['src', ...fileNameArray, fileFullName];
+  } else {
+    // 1
+    fileFullPath = ['src', fileName, fileFullName];
+  }
+
+  return path.join(...fileFullPath);
+};
+
+module.exports = { getGeneratedFilePath };
--- generator/component/component.generator.js
+++ generator/component/component.generator.js
@@ -1,9 +1,15 @@
+const { getGeneratedFilePath } = require('../app/app.service');
+
 const componentGenerator = (api, options) => {
   if (!options.component) return;
 
-  api.render({
-    'src/components/demo.vue': './templates/component.ejs',
-  });
+  // 组件的存放位置
+  const generatedComponentPath = getGeneratedFilePath('component', options);
+  console.log(generatedComponentPath);
+
+  // api.render({
+  //   'src/components/demo.vue': './templates/component.ejs',
+  // });
 };
 
 module.exports = componentGenerator;
