4439f59d11e2c0c8b104259d69a57193221c279c
wanghao <wanghao@ninghao.net>
2020-12-02 09:42:22 +0800

    独立出 Store 模块生成器钩子

--- generator/store/store.generator.hook.js
+++ generator/store/store.generator.hook.js
@@ -14,52 +14,59 @@ const { getStoreStateName, getStoreModuleName } = require('./store.service');
  */
 const storeGeneratorHook = (api, options) => {
   api.afterInvoke(() => {
-    if (!options.store || !options.parent || !options.module) return;
-    const { module: moduleName, parent: parentStore } = options;
+    const { store, parent, module } = options;
 
-    // Store 数据类型
-    const storeStateName = getStoreStateName(options);
-
-    // Store 模块名
-    const storeModuleName = getStoreModuleName(options);
-
-    // 父 Store 路径
-    const parentStorePath = getParentFilePath('store', options);
-
-    // 父 Store 文件内容
-    let parentFileContent = getProjectFileContent(parentStorePath, api);
-
-    // 查找父 Store 文件
-    let findParentStoreState;
-
-    if (parentStore === 'app/app') {
-      findParentStoreState = 'export interface RootState';
-    } else {
-      findParentStoreState = 'export interface .+StoreState';
+    // Store 模块生成器钩子
+    if (store && parent && module) {
+      storeModuleGeneratorHook(api, options);
     }
+  });
+};
 
-    // 在父 Store 类型里插入模块
-    parentFileContent = insertFileContent({
-      fileContent: parentFileContent,
-      find: findParentStoreState,
-      insert: `${EOL}  ${moduleName}: ${storeStateName},`,
-    });
+/**
+ * Store 模块生成器钩子
+ */
+const storeModuleGeneratorHook = (api, options) => {
+  const { module: moduleName, parent: parentStore } = options;
 
-    // 在父 Store 模块里注册模块
-    parentFileContent = insertFileContent({
-      fileContent: parentFileContent,
-      find: 'modules: {',
-      insert: `${EOL}    ${moduleName}: ${storeModuleName},`,
-    });
+  // Store 数据类型
+  const storeStateName = getStoreStateName(options);
 
-    // 写入父 Store 模块文件
-    fs.writeFileSync(
-      api.resolve(parentStorePath),
-      parentFileContent.join(EOL),
-      {
-        encoding: 'utf-8',
-      },
-    );
+  // Store 模块名
+  const storeModuleName = getStoreModuleName(options);
+
+  // 父 Store 路径
+  const parentStorePath = getParentFilePath('store', options);
+
+  // 父 Store 文件内容
+  let parentFileContent = getProjectFileContent(parentStorePath, api);
+
+  // 查找父 Store 文件
+  let findParentStoreState;
+
+  if (parentStore === 'app/app') {
+    findParentStoreState = 'export interface RootState';
+  } else {
+    findParentStoreState = 'export interface .+StoreState';
+  }
+
+  // 在父 Store 类型里插入模块
+  parentFileContent = insertFileContent({
+    fileContent: parentFileContent,
+    find: findParentStoreState,
+    insert: `${EOL}  ${moduleName}: ${storeStateName},`,
+  });
+
+  // 在父 Store 模块里注册模块
+  parentFileContent = insertFileContent({
+    fileContent: parentFileContent,
+    find: 'modules: {',
+    insert: `${EOL}    ${moduleName}: ${storeModuleName},`,
+  });
+
+  // 写入父 Store 模块文件
+  fs.writeFileSync(api.resolve(parentStorePath), parentFileContent.join(EOL), {
+    encoding: 'utf-8',
   });
 };
 
